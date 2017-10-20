1f7fd24a9378785a8e3ac96a872d9c9f406a97ec
Marcel <vester.marcel@googlemail.com>
2017-10-20 16:30:06 +0200

    Test 2

--- gittest.java
+++ gittest.java
@@ -5,16 +5,22 @@ import org.junit.Test;
 
 public class gittest {
 	
-	Project test_one;
+	Project test_one , test_two;
 	
 	@Before
 	public void setUp(){
 		test_one = new Project(new String[][]{{"A" , "D"} , {"C" , "E"}});
+		test_two = new Project(new String[][]{{"A", "A"}});
 	}
 
 	@Test
 	public void test_case_1() {
 		assertTrue(test_one.isWellSorted(new String[]{"A", "B", "C", "D", "E"}));
+	}
+	
+	@Test
+	public void test_case_2(){
+		assertFalse(test_two.isWellSorted(new String[]{"A"}));
 	}
 
 }
