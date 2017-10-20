f12a47fcb868921d209f82590a6f046d8e7179e7
pasca <pascal_p98@outlook.de>
2017-10-20 15:46:07 +0200

    Lösung des Tests

---
+++ Project.java
@@ -0,0 +1,23 @@
+
+public class Project {
+
+	String[][] relation;
+	
+	public Project(String[][] relation){
+		this.relation = relation;
+	}
+	
+	public boolean isWellSorted ( String[] sequence){
+		for(int i=0;i<sequence.length;++i){
+			for(int j=1+1;j<sequence.length;++j){
+				for(int k=0;k<relation.length;++k){
+					if(sequence[i].equals(relation[k][1]) && sequence[j].equals(relation[k][0])){
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
+	
+}
